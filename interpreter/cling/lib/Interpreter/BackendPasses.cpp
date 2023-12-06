ce45d2b015cd7d033ee9e48e48c0b8e85be18498
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2023-12-06 11:37:44 +0100

    Hybrid pass manager approach

--- interpreter/cling/lib/Interpreter/BackendPasses.cpp
+++ interpreter/cling/lib/Interpreter/BackendPasses.cpp
@@ -26,6 +26,7 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils.h"
+#include "llvm/Passes/PassPlugin.h"
 
 //#include "clang/Basic/LangOptions.h"
 //#include "clang/Basic/TargetOptions.h"
@@ -346,119 +347,163 @@ BackendPasses::~BackendPasses() {
 
 void BackendPasses::CreatePasses(llvm::Module& M, int OptLevel)
 {
-  // From BackEndUtil's clang::EmitAssemblyHelper::CreatePasses().
+  // // From BackEndUtil's clang::EmitAssemblyHelper::CreatePasses().
 
-#if 0
-  CodeGenOptions::InliningMethod Inlining = m_CGOpts.getInlining();
-  CodeGenOptions& CGOpts_ = const_cast<CodeGenOptions&>(m_CGOpts);
-  // DON'T: we will not find our symbols...
-  //CGOpts_.CXXCtorDtorAliases = 1;
+// #if 0
+//   CodeGenOptions::InliningMethod Inlining = m_CGOpts.getInlining();
+//   CodeGenOptions& CGOpts_ = const_cast<CodeGenOptions&>(m_CGOpts);
+//   // DON'T: we will not find our symbols...
+//   //CGOpts_.CXXCtorDtorAliases = 1;
 
-  // Default clang -O2 on Linux 64bit also has the following, but see
-  // CIFactory.cpp.
-  CGOpts_.DisableFPElim = 0;
-  CGOpts_.DiscardValueNames = 1;
-  CGOpts_.OmitLeafFramePointer = 1;
-  CGOpts_.OptimizationLevel = 2;
-  CGOpts_.RelaxAll = 0;
-  CGOpts_.UnrollLoops = 1;
-  CGOpts_.VectorizeLoop = 1;
-  CGOpts_.VectorizeSLP = 1;
-#endif
+//   // Default clang -O2 on Linux 64bit also has the following, but see
+//   // CIFactory.cpp.
+//   CGOpts_.DisableFPElim = 0;
+//   CGOpts_.DiscardValueNames = 1;
+//   CGOpts_.OmitLeafFramePointer = 1;
+//   CGOpts_.OptimizationLevel = 2;
+//   CGOpts_.RelaxAll = 0;
+//   CGOpts_.UnrollLoops = 1;
+//   CGOpts_.VectorizeLoop = 1;
+//   CGOpts_.VectorizeSLP = 1;
+// #endif
 
-#if 0 // def __GNUC__
-  // Better inlining is pending https://bugs.llvm.org//show_bug.cgi?id=19668
-  // and its consequence https://sft.its.cern.ch/jira/browse/ROOT-7111
-  // shown e.g. by roottest/cling/stl/map/badstringMap
-  if (Inlining > CodeGenOptions::NormalInlining)
-    Inlining = CodeGenOptions::NormalInlining;
-#endif
+// #if 0 // def __GNUC__
+//   // Better inlining is pending https://bugs.llvm.org//show_bug.cgi?id=19668
+//   // and its consequence https://sft.its.cern.ch/jira/browse/ROOT-7111
+//   // shown e.g. by roottest/cling/stl/map/badstringMap
+//   if (Inlining > CodeGenOptions::NormalInlining)
+//     Inlining = CodeGenOptions::NormalInlining;
+// #endif
 
-  // Handle disabling of LLVM optimization, where we want to preserve the
-  // internal module before any optimization.
-  if (m_CGOpts.DisableLLVMPasses) {
-    OptLevel = 0;
-    // Always keep at least ForceInline - NoInlining is deadly for libc++.
-    // Inlining = CGOpts.NoInlining;
+//   // Handle disabling of LLVM optimization, where we want to preserve the
+//   // internal module before any optimization.
+//   if (m_CGOpts.DisableLLVMPasses) {
+//     OptLevel = 0;
+//     // Always keep at least ForceInline - NoInlining is deadly for libc++.
+//     // Inlining = CGOpts.NoInlining;
+//   }
+
+//   llvm::PassManagerBuilder PMBuilder;
+//   PMBuilder.OptLevel = OptLevel;
+//   PMBuilder.SizeLevel = m_CGOpts.OptimizeSize;
+//   PMBuilder.SLPVectorize = OptLevel > 1 ? 1 : 0; // m_CGOpts.VectorizeSLP
+//   PMBuilder.LoopVectorize = OptLevel > 1 ? 1 : 0; // m_CGOpts.VectorizeLoop
+
+//   PMBuilder.DisableUnrollLoops = !m_CGOpts.UnrollLoops;
+//   PMBuilder.MergeFunctions = m_CGOpts.MergeFunctions;
+
+//   PMBuilder.LibraryInfo = new TargetLibraryInfoImpl(m_TM.getTargetTriple());
+
+//   // At O0 and O1 we only run the always inliner which is more efficient. At
+//   // higher optimization levels we run the normal inliner.
+//   // See also call to `CGOpts.setInlining()` in CIFactory!
+//   if (PMBuilder.OptLevel <= 1) {
+//     bool InsertLifetimeIntrinsics = PMBuilder.OptLevel != 0;
+//     PMBuilder.Inliner = createAlwaysInlinerLegacyPass(InsertLifetimeIntrinsics);
+//   } else {
+//     PMBuilder.Inliner = createFunctionInliningPass(OptLevel,
+//                                                    PMBuilder.SizeLevel,
+//             (!m_CGOpts.SampleProfileFile.empty() && m_CGOpts.PrepareForThinLTO));
+//   }
+
+//   // Set up the per-module pass manager.
+//   m_MPM[OptLevel].reset(new legacy::PassManager());
+
+//   m_MPM[OptLevel]->add(new KeepLocalGVPass());
+//   m_MPM[OptLevel]->add(new PreventLocalOptPass());
+//   m_MPM[OptLevel]->add(new WeakTypeinfoVTablePass());
+//   m_MPM[OptLevel]->add(new ReuseExistingWeakSymbols(m_JIT));
+
+//   // The function __cuda_module_ctor and __cuda_module_dtor will just generated,
+//   // if a CUDA fatbinary file exist. Without file path there is no need for the
+//   // function pass.
+//   if(!m_CGOpts.CudaGpuBinaryFileName.empty())
+//     m_MPM[OptLevel]->add(new UniqueCUDAStructorName());
+//   m_MPM[OptLevel]->add(createTargetTransformInfoWrapperPass(
+//                                                    m_TM.getTargetIRAnalysis()));
+
+//   //if (!CGOpts.RewriteMapFiles.empty())
+//   //  addSymbolRewriterPass(CGOpts, m_MPM);
+
+//   PMBuilder.populateModulePassManager(*m_MPM[OptLevel]);
+
+//   m_FPM[OptLevel].reset(new legacy::FunctionPassManager(&M));
+//   m_FPM[OptLevel]->add(createTargetTransformInfoWrapperPass(
+//                                                    m_TM.getTargetIRAnalysis()));
+//   if (m_CGOpts.VerifyModule)
+//       m_FPM[OptLevel]->add(createVerifierPass());
+//   PMBuilder.populateFunctionPassManager(*m_FPM[OptLevel]);
+
+  PassBuilder nPB;
+
+  nPB.registerModuleAnalyses(m_MAM[OptLevel]);
+  nPB.registerCGSCCAnalyses(m_CGAM[OptLevel]);
+  nPB.registerFunctionAnalyses(m_FAM[OptLevel]);
+  nPB.registerLoopAnalyses(m_LAM[OptLevel]);
+  nPB.crossRegisterProxies(m_LAM[OptLevel], m_FAM[OptLevel],
+                           m_CGAM[OptLevel], m_MAM[OptLevel]);
+
+  auto plugin = PassPlugin::Load("/usr/local/lib/libacpp-clang.so");
+
+  if (plugin) {
+    plugin.get().registerPassBuilderCallbacks(nPB);
+  }
+  
+  OptimizationLevel level;
+
+  switch (OptLevel) {
+  case 0:
+  default:
+    level = OptimizationLevel::O0;
+    break;
+
+  case 1:
+    level = OptimizationLevel::O1;
+    break;
+
+  case 2:
+    level = OptimizationLevel::O2;
+    break;
+
+  case 3:
+    level = OptimizationLevel::O3;
+    break;
   }
 
-  llvm::PassManagerBuilder PMBuilder;
-  PMBuilder.OptLevel = OptLevel;
-  PMBuilder.SizeLevel = m_CGOpts.OptimizeSize;
-  PMBuilder.SLPVectorize = OptLevel > 1 ? 1 : 0; // m_CGOpts.VectorizeSLP
-  PMBuilder.LoopVectorize = OptLevel > 1 ? 1 : 0; // m_CGOpts.VectorizeLoop
-
-  PMBuilder.DisableUnrollLoops = !m_CGOpts.UnrollLoops;
-  PMBuilder.MergeFunctions = m_CGOpts.MergeFunctions;
-
-  PMBuilder.LibraryInfo = new TargetLibraryInfoImpl(m_TM.getTargetTriple());
-
-  // At O0 and O1 we only run the always inliner which is more efficient. At
-  // higher optimization levels we run the normal inliner.
-  // See also call to `CGOpts.setInlining()` in CIFactory!
-  if (PMBuilder.OptLevel <= 1) {
-    bool InsertLifetimeIntrinsics = PMBuilder.OptLevel != 0;
-    PMBuilder.Inliner = createAlwaysInlinerLegacyPass(InsertLifetimeIntrinsics);
-  } else {
-    PMBuilder.Inliner = createFunctionInliningPass(OptLevel,
-                                                   PMBuilder.SizeLevel,
-            (!m_CGOpts.SampleProfileFile.empty() && m_CGOpts.PrepareForThinLTO));
-  }
-
-  // Set up the per-module pass manager.
-  m_MPM[OptLevel].reset(new legacy::PassManager());
-
-  m_MPM[OptLevel]->add(new KeepLocalGVPass());
-  m_MPM[OptLevel]->add(new PreventLocalOptPass());
-  m_MPM[OptLevel]->add(new WeakTypeinfoVTablePass());
-  m_MPM[OptLevel]->add(new ReuseExistingWeakSymbols(m_JIT));
-
-  // The function __cuda_module_ctor and __cuda_module_dtor will just generated,
-  // if a CUDA fatbinary file exist. Without file path there is no need for the
-  // function pass.
-  if(!m_CGOpts.CudaGpuBinaryFileName.empty())
-    m_MPM[OptLevel]->add(new UniqueCUDAStructorName());
-  m_MPM[OptLevel]->add(createTargetTransformInfoWrapperPass(
-                                                   m_TM.getTargetIRAnalysis()));
-
-  //if (!CGOpts.RewriteMapFiles.empty())
-  //  addSymbolRewriterPass(CGOpts, m_MPM);
-
-  PMBuilder.populateModulePassManager(*m_MPM[OptLevel]);
-
-  m_FPM[OptLevel].reset(new legacy::FunctionPassManager(&M));
-  m_FPM[OptLevel]->add(createTargetTransformInfoWrapperPass(
-                                                   m_TM.getTargetIRAnalysis()));
-  if (m_CGOpts.VerifyModule)
-      m_FPM[OptLevel]->add(createVerifierPass());
-  PMBuilder.populateFunctionPassManager(*m_FPM[OptLevel]);
+  m_nMPM[OptLevel] = nPB.buildPerModuleDefaultPipeline(level);
 }
 
 void BackendPasses::runOnModule(Module& M, int OptLevel) {
 
-  if (OptLevel < 0)
-    OptLevel = 0;
-  if (OptLevel > 3)
-    OptLevel = 3;
+  // if (OptLevel < 0)
+  //   OptLevel = 0;
+  // if (OptLevel > 3)
+  //   OptLevel = 3;
 
-  if (!m_MPM[OptLevel])
-    CreatePasses(M, OptLevel);
+  // if (!m_MPM[OptLevel])
+  CreatePasses(M, OptLevel);
 
-  static constexpr std::array<llvm::CodeGenOpt::Level, 4> CGOptLevel {{
-    llvm::CodeGenOpt::None,
-    llvm::CodeGenOpt::Less,
-    llvm::CodeGenOpt::Default,
-    llvm::CodeGenOpt::Aggressive
-  }};
-  // TM's OptLevel is used to build orc::SimpleCompiler passes for every Module.
-  m_TM.setOptLevel(CGOptLevel[OptLevel]);
+  // static constexpr std::array<llvm::CodeGenOpt::Level, 4> CGOptLevel {{
+  //   llvm::CodeGenOpt::None,
+  //   llvm::CodeGenOpt::Less,
+  //   llvm::CodeGenOpt::Default,
+  //   llvm::CodeGenOpt::Aggressive
+  // }};
+  // // TM's OptLevel is used to build orc::SimpleCompiler passes for every Module.
+  // m_TM.setOptLevel(CGOptLevel[OptLevel]);
 
-  // Run the per-function passes on the module.
-  m_FPM[OptLevel]->doInitialization();
-  for (auto&& I: M.functions())
-    if (!I.isDeclaration())
-      m_FPM[OptLevel]->run(I);
-  m_FPM[OptLevel]->doFinalization();
+  // // Run the per-function passes on the module.
+  
+  
+  // m_FPM[OptLevel]->doInitialization();
+  // for (auto&& I: M.functions())
+  //   if (!I.isDeclaration())
+  //     m_FPM[OptLevel]->run(I);
+  // m_FPM[OptLevel]->doFinalization();
 
-  m_MPM[OptLevel]->run(M);
+  
+
+  // m_MPM[OptLevel]->run(M);
+  
+  m_nMPM[OptLevel].run(M, m_MAM[OptLevel]);
 }
--- interpreter/cling/lib/Interpreter/BackendPasses.h
+++ interpreter/cling/lib/Interpreter/BackendPasses.h
@@ -11,6 +11,11 @@
 #define CLING_BACKENDPASSES_H
 
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Analysis/LoopAnalysisManager.h"
+#include "llvm/Analysis/CGSCCPassManager.h"
+#include "llvm/Passes/OptimizationLevel.h"
+#include "llvm/Passes/PassBuilder.h"
 
 #include <array>
 #include <memory>
@@ -42,6 +47,12 @@ namespace cling {
   class BackendPasses {
     std::array<std::unique_ptr<llvm::legacy::PassManager>, 4> m_MPM;
     std::array<std::unique_ptr<llvm::legacy::FunctionPassManager>, 4> m_FPM;
+
+    std::array<llvm::ModulePassManager, 4> m_nMPM;
+    std::array<llvm::LoopAnalysisManager, 4> m_LAM;
+    std::array<llvm::FunctionAnalysisManager, 4> m_FAM;
+    std::array<llvm::CGSCCAnalysisManager, 4> m_CGAM;
+    std::array<llvm::ModuleAnalysisManager, 4> m_MAM;
 
     llvm::TargetMachine& m_TM;
     IncrementalJIT &m_JIT;

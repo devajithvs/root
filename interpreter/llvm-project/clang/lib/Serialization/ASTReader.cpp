d222637cc3ec1d8a056cc23cb3a30abe9693dfe4
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-02-29 18:25:30 +0000

    [Serialization] Code cleanups and polish 83233

--- interpreter/llvm-project/clang/lib/Serialization/ASTReader.cpp
+++ interpreter/llvm-project/clang/lib/Serialization/ASTReader.cpp
@@ -3529,7 +3529,7 @@ llvm::Error ASTReader::ReadASTBlock(ModuleFile &F,
       break;
     }
 
-    case UPDATE_SPECIALIZATION: {
+    case CXX_ADDED_TEMPLATE_SPECIALIZATION: {
       unsigned Idx = 0;
       serialization::DeclID ID = ReadDeclID(F, Record, Idx);
       auto *Data = (const unsigned char *)Blob.data();
@@ -8025,7 +8025,7 @@ void ASTReader::LoadExternalSpecializations(
     return;
 
   Deserializing LookupResults(this);
-  auto HashValue = TemplateArgumentList::ComputeODRHash(TemplateArgs);
+  auto HashValue = TemplateArgumentList::ComputeStableHash(TemplateArgs);
 
   // Get Decl may violate the iterator from SpecializationsLookups
   llvm::SmallVector<serialization::reader::LazySpecializationInfo, 8> Infos;

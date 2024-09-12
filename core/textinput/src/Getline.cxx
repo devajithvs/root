3802a8044c83bc51f85d8ca94a2efce48ddf25df
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2024-09-13 00:41:49 +0200

    First set of changes
    Revert "Remove textinput folder"

--- core/textinput/src/Getline.cxx
+++ core/textinput/src/Getline.cxx
@@ -15,13 +15,6 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
-#include "textinput/TextInput.h"
-#include "textinput/TextInputContext.h"
-#include "textinput/History.h"
-#include "textinput/TerminalDisplay.h"
-#include "textinput/StreamReader.h"
-#include "textinput/Callbacks.h"
-#include "Getline_color.h"
 #include "TApplication.h"
 #include "TInterpreter.h"
 
@@ -30,240 +23,192 @@ extern "C" {
    int (* Gl_beep_hook)() = nullptr;
 }
 
+#include "llvm/LineEditor/LineEditor.h"
 
-using namespace textinput;
+// using namespace textinput;
 
 namespace {
-   // TTabCom adapter.
-   class ROOTTabCompletion: public TabCompletion {
+   class ROOTTabCompletion {
    public:
-      ROOTTabCompletion(): fLineBuf(new char[fgLineBufSize]) {}
-      virtual ~ROOTTabCompletion() { delete []fLineBuf; }
+      std::vector<llvm::LineEditor::Completion> operator()(llvm::StringRef Buffer, size_t Pos) const {
+         char fLineBuf[16 * 1024]; // Buffer size
+         std::vector<std::string> displayCompletions;
 
-      ROOTTabCompletion(const ROOTTabCompletion&) = delete;
-      ROOTTabCompletion& operator=(const ROOTTabCompletion&) = delete;
-
-      // Returns false on error
-      bool Complete(Text& line /*in+out*/, size_t& cursor /*in+out*/,
-                    EditorRange& r /*out*/,
-                    std::vector<std::string>& displayCompletions /*out*/) override {
-         strlcpy(fLineBuf, line.GetText().c_str(), fgLineBufSize);
-         int cursorInt = (int) cursor;
+         // Invoke the application’s tab completion mechanism
          std::stringstream sstr;
+         int cursorInt = (int)Pos;
          size_t posFirstChange = gApplication->TabCompletionHook(fLineBuf, &cursorInt, sstr);
-         if (posFirstChange == (size_t) -1) {
-            // no change
-            return true;
+
+         if (posFirstChange == (size_t)-1) {
+               return {};  // No completions available
          }
 
-         line = std::string(fLineBuf);
          std::string compLine;
          while (std::getline(sstr, compLine)) {
-            displayCompletions.push_back(compLine);
+               displayCompletions.push_back(compLine);
          }
-         std::sort(displayCompletions.begin(), displayCompletions.end());
 
-         size_t lenLineBuf = strlen(fLineBuf);
-         if (posFirstChange == (size_t) -2) {
-            // redraw whole line, incl prompt
-            r.fEdit.Extend(Range::AllWithPrompt());
-            r.fDisplay.Extend(Range::AllWithPrompt());
-         } else {
-            if (!lenLineBuf) {
-               r.fEdit.Extend(Range::AllText());
-               r.fDisplay.Extend(Range::AllText());
-            } else {
-               r.fEdit.Extend(Range(posFirstChange, Range::End()));
-               r.fDisplay.Extend(Range(posFirstChange, Range::End()));
-            }
+         std::vector<llvm::LineEditor::Completion> results;
+         for (const auto& comp : displayCompletions) {
+               results.emplace_back(comp, comp);
          }
-         cursor = (size_t)cursorInt;
-         line.GetColors().resize(lenLineBuf);
-         return true;
-      }
-   private:
-      static const size_t fgLineBufSize;
-      char* fLineBuf;
-   };
-   const size_t ROOTTabCompletion::fgLineBufSize = 16 * 1024; // must be equal to/larger than BUF_SIZE in TTabCom.cxx
 
-   class TClingTabCompletion: public TabCompletion {
-   public:
-      TClingTabCompletion() {}
-      virtual ~TClingTabCompletion() {}
-
-      TClingTabCompletion(const TClingTabCompletion&) = delete;
-      TClingTabCompletion& operator=(const TClingTabCompletion&) = delete;
-
-      // Returns false on error
-      bool Complete(Text& line /*in+out*/, size_t& cursor /*in+out*/,
-                    EditorRange& r /*out*/,
-                    std::vector<std::string>& completions /*out*/) override {
-         gInterpreter->CodeComplete(line.GetText(), cursor, completions);
-         // FIXME: handle single completion by completing "line"
-         // FIXME: adjust r's update range, for now:
-         // redraw whole line, incl prompt
-         r.fEdit.Extend(Range::AllWithPrompt());
-         r.fDisplay.Extend(Range::AllWithPrompt());
-         return true;
+         return results;
       }
    };
+
 
    // Helper to define the lifetime of the TextInput singleton.
-   class TextInputHolder {
+   class LineEditorHolder {
    public:
-      TextInputHolder():
-         fTextInput(*(fReader = StreamReader::Create()),
-                    *(fDisplay = TerminalDisplay::Create()),
-                    fgHistoryFile.c_str()) {
-         fTextInput.SetColorizer(&fCol);
-         fTextInput.SetCompletion(&fTabComp);
-         fTextInput.EnableAutoHistAdd(false);
-         History* Hist = fTextInput.GetContext()->GetHistory();
-         Hist->SetMaxDepth(fgSizeLines);
-         Hist->SetPruneLength(fgSaveLines);
-      }
+   LineEditorHolder(const std::string& historyFile)
+        : LE("root-repl", historyFile.c_str()), historyFile(historyFile) {
+   }
 
-      ~TextInputHolder() {
-         // Delete allocated objects.
-         delete fReader;
-         delete fDisplay;
-      }
+      ~LineEditorHolder() = default;
 
+      // Take input from the user, returns the input line
       const char* TakeInput(bool force = false) {
-         fTextInput.TakeInput(fInputLine, force);
-         fInputLine += "\n"; // ROOT wants a trailing newline.
+         std::optional<std::string> Input = LE.readLine();
+         if (Input) {
+               fInputLine = *Input + "\n"; // ROOT wants trailing newline
+         } else {
+            fInputLine.clear();
+         }
          return fInputLine.c_str();
       }
 
       void SetColors(const char* colorTab, const char* colorTabComp,
                      const char* colorBracket, const char* colorBadBracket,
                      const char* colorPrompt) {
-         fCol.SetColors(colorTab, colorTabComp, colorBracket, colorBadBracket,
-                        colorPrompt);
+         // This part depends on whether you want to use any custom coloring in LineEditor
+         // Custom color support can be configured for prompts or completions here
+         // LE.setPromptColor(colorPrompt); // Example of setting prompt color
       }
 
       static void SetHistoryFile(const char* hist) {
          fgHistoryFile = hist;
       }
+
       static void SetHistSize(int size, int save) {
          fgSizeLines = size;
          fgSaveLines = save;
       }
 
-      static TextInputHolder& getHolder() {
-         // Controls initialization of static TextInput.
-         static TextInputHolder sTIHolder;
-         return sTIHolder;
+      static LineEditorHolder& getHolder() {
+         static LineEditorHolder sLEHolder(fgHistoryFile);
+         return sLEHolder;
       }
 
-      static TextInput& get() {
-         return getHolder().fTextInput;
+      llvm::LineEditor& get() {
+         return LE;
       }
 
    private:
-      TextInput fTextInput; // The singleton TextInput object.
-      Display* fDisplay; // Default TerminalDisplay
-      Reader* fReader; // Default StreamReader
-      std::string fInputLine; // Taken from TextInput
-      ROOT::TextInputColorizer fCol; // Colorizer
-      ROOTTabCompletion fTabComp; // Tab completion handler / TTabCom adapter
+      llvm::LineEditor LE;  // LineEditor instance
+      std::string fInputLine;  // Stores the input line taken from user
+      std::string historyFile;  // Path to history file
 
-      // Config values:
       static std::string fgHistoryFile;
-      // # History file size, once HistSize is reached remove all but HistSave entries,
-      // # set to 0 to turn off command recording.
-      // # Can be overridden by environment variable ROOT_HIST=size[:save],
-      // # the ":save" part is optional.
-      // # Rint.HistSize:         500
-      // # Set to -1 for sensible default (80% of HistSize), set to 0 to disable history.
-      // # Rint.HistSave:         400
       static int fgSizeLines;
       static int fgSaveLines;
    };
 
-   int TextInputHolder::fgSizeLines = 500;
-   int TextInputHolder::fgSaveLines = -1;
-   std::string TextInputHolder::fgHistoryFile;
+   std::string LineEditorHolder::fgHistoryFile;
+   int LineEditorHolder::fgSizeLines = 500;
+   int LineEditorHolder::fgSaveLines = -1;
 }
 
 /************************ extern "C" part *********************************/
 
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+
+void SetEcho(bool enableEcho) {
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (!enableEcho) {
+        tty.c_lflag &= ~ECHO;  // Disable echoing
+    } else {
+        tty.c_lflag |= ECHO;  // Enable echoing
+    }
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);  // Apply the settings
+}
+
 extern "C" {
 void
 Gl_config(const char* which, int value) {
-   if (strcmp(which, "noecho") == 0) {
-      TextInputHolder::get().MaskInput(value);
-   } else {
-      // unsupported directive
-      printf("Gl_config unsupported: %s ?\n", which);
-   }
+    if (strcmp(which, "noecho") == 0) {
+        SetEcho(value == 0);  // Disable echo if value == 0
+    } else {
+        printf("Gl_config unsupported: %s ?\n", which);
+    }
 }
 
-void
-Gl_histadd(const char* buf) {
-   TextInputHolder::get().AddHistoryLine(buf);
+void Gl_histadd(const char* buf) {
+   // llvm::LineEditor handles history automatically.
 }
 
 /* Wrapper around textinput.
  * Modes: -1 = init, 0 = line mode, 1 = one char at a time mode, 2 = cleanup, 3 = clear input line
  */
-const char *Getlinem(EGetLineMode mode, const char* prompt) {
+const char* Getlinem(EGetLineMode mode, const char* prompt) {
+    static std::string inputLine;
 
-   if (mode == kClear) {
-      TextInputHolder::getHolder().TakeInput(true);
-      return nullptr;
-   }
+    if (mode == kClear) {
+        inputLine.clear(); // Clear input buffer
+        return nullptr;
+    }
 
-   if (mode == kCleanUp) {
-      TextInputHolder::get().ReleaseInputOutput();
-      return nullptr;
-   }
+    if (mode == kCleanUp) {
+        // Cleanup actions
+        return nullptr;
+    }
 
-   if (mode == kOneChar) {
-      // Check first display: if !TTY, read full line.
-      const textinput::Display* disp
-         = TextInputHolder::get().GetContext()->GetDisplays()[0];
-      const textinput::TerminalDisplay* tdisp = nullptr;
-      if (disp) tdisp = dynamic_cast<const textinput::TerminalDisplay*>(disp);
-      if (tdisp && !tdisp->IsTTY()) {
-         mode = kLine1;
-      }
-   }
+    if (mode == kInit || mode == kLine1) {
+        llvm::LineEditor& editor = LineEditorHolder::getHolder().get();
+        editor.setPrompt(prompt ? prompt : "");
 
-   if (mode == kInit || mode == kLine1) {
-      if (prompt) {
-         TextInputHolder::get().SetPrompt(prompt);
-      }
-      // Trigger attach:
-      TextInputHolder::get().Redraw();
-      if (mode == kInit) {
-         return nullptr;
-      }
-      TextInputHolder::get().SetBlockingUntilEOL();
-   } else {
-      // mode == kOneChar
-      if (Gl_in_key) {
-         // We really need to go key by key:
-         TextInputHolder::get().SetMaxPendingCharsToRead(1);
-      } else {
-         // Can consume all pending characters
-         TextInputHolder::get().SetReadingAllPendingChars();
-      }
-   }
+        std::optional<std::string> line = editor.readLine();
+        if (line) {
+            inputLine = *line + "\n";  // Add newline for ROOT
 
-   TextInput::EReadResult res = TextInputHolder::get().ReadInput();
-   if (Gl_in_key) {
-      (*Gl_in_key)(TextInputHolder::get().GetLastKey());
-   }
-   if (res == TextInput::kRRReadEOLDelimiter) {
-      return TextInputHolder::getHolder().TakeInput();
-   } else if (res == TextInput::kRREOF) {
-      // ROOT expects "" and then Gl_eol() returning true
-      return "";
-   }
+            // If the command is '.q' or '.quit', exit
+            if (inputLine == ".q\n" || inputLine == ".quit\n") {
+                std::exit(0);  // Exit the program
+            }
 
-   return nullptr;
+            // Add the line to history if necessary
+            Gl_histadd(inputLine.c_str());
+
+            // **Process the input using the ROOT interpreter**
+            // This is important to ensure that the commands are executed.
+            gInterpreter->ProcessLine(inputLine.c_str());
+
+            return inputLine.c_str();  // Return the input line with a newline
+        }
+
+        return nullptr;
+    }
+
+    if (mode == kOneChar) {
+        // Implement single-character input reading
+        struct termios oldt, newt;
+        tcgetattr(STDIN_FILENO, &oldt);  // Get terminal attributes
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);  // Disable canonical mode and echoing
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+        char ch;
+        read(STDIN_FILENO, &ch, 1);  // Read one character from stdin
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  // Restore terminal attributes
+        inputLine = ch;  // Store the single character input
+        return inputLine.c_str();
+    }
+
+    return nullptr;
 }
 
 const char*
@@ -286,25 +231,31 @@ Getline(const char* prompt) {
 
 void
 Gl_histsize(int size, int save) {
-   TextInputHolder::SetHistSize(size, save);
+   LineEditorHolder::SetHistSize(size, save);
 }
 
 void
 Gl_histinit(const char* file) {
-   // Has to be called before constructing TextInputHolder singleton.
-   TextInputHolder::SetHistoryFile(file);
+   // Has to be called before constructing LineEditorHolder singleton.
+   LineEditorHolder::SetHistoryFile(file);
 }
 
-int
-Gl_eof() {
-   return TextInputHolder::get().GetReadState() == TextInput::kRREOF;
+int Gl_eof() {
+    // Check for EOF using std::optional from LineEditor
+    llvm::LineEditor& editor = LineEditorHolder::getHolder().get();
+    std::optional<std::string> line = editor.readLine();
+    if (!line) {
+        return 1;  // EOF detected
+    }
+    return 0;
 }
+
 
 void
 Gl_setColors(const char* colorTab, const char* colorTabComp, const char* colorBracket,
              const char* colorBadBracket, const char* colorPrompt) {
    // call to enhance.cxx to set colours
-   TextInputHolder::getHolder().SetColors(colorTab, colorTabComp, colorBracket,
+   LineEditorHolder::getHolder().SetColors(colorTab, colorTabComp, colorBracket,
                                           colorBadBracket, colorPrompt);
 }
 

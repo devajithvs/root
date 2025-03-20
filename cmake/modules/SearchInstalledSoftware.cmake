6ae4f48460aadbbdcd080ac42fb95801c74d6508
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-03-20 14:17:07 +0100

    Some cleanups

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -62,7 +62,7 @@ macro(ROOT_CHECK_CONNECTION_AND_DISABLE_OPTION option_name)
 endmacro()
 
 # Building adaptivecpp requires an internet connection, if we're not side-loading the source directory
-if(adaptivecpp)
+if(adaptivecpp AND NOT DEFINED ADAPTIVECPP_SOURCE_DIR)
   ROOT_CHECK_CONNECTION_AND_DISABLE_OPTION("adaptivecpp")
 endif()
 

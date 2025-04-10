c60bc20024be7d54b71b28533c80f6504c599ceb
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-04-10 09:43:32 +0200

    [adaptivecpp] Rename adaptivecpp option to experimental_adaptivecpp

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -62,8 +62,8 @@ macro(ROOT_CHECK_CONNECTION_AND_DISABLE_OPTION option_name)
 endmacro()
 
 # Building adaptivecpp requires an internet connection, if we're not side-loading the source directory
-if(adaptivecpp AND NOT DEFINED ADAPTIVECPP_SOURCE_DIR)
-  ROOT_CHECK_CONNECTION_AND_DISABLE_OPTION("adaptivecpp")
+if(experimental_adaptivecpp AND NOT DEFINED ADAPTIVECPP_SOURCE_DIR)
+  ROOT_CHECK_CONNECTION_AND_DISABLE_OPTION("experimental_adaptivecpp")
 endif()
 
 # Building Clad requires an internet connection, if we're not side-loading the source directory

1066e591e8e7e5f47a38303bbe47e897cc081c39
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-09-10 12:06:40 +0200

    [backport][math] add DeltaR to GenVector
    Changes similar to commit dd272abf889e49b6eddc8f08a5383530dd421cb9

--- math/experimental/genvectorx/inc/MathX/GenVectorX/LorentzVector.h
+++ math/experimental/genvectorx/inc/MathX/GenVectorX/LorentzVector.h
@@ -24,6 +24,8 @@
 
 #include "MathX/GenVectorX/GenVectorIO.h"
 
+#include "TMath.h"
+
 #include "MathX/GenVectorX/MathHeaders.h"
 
 #include "MathX/GenVectorX/AccHeaders.h"
@@ -363,6 +365,29 @@ public:
       \f[ \eta = - \ln { \tan { \frac { \theta} {2} } } \f]
    */
    Scalar Eta() const { return fCoordinates.Eta(); }
+
+   /**
+      deltaRapidity between this and vector v
+      \f[ \Delta R = \sqrt { \Delta \eta ^2 + \Delta \phi ^2 } \f]
+      \param useRapidity true to use Rapidity(), false to use Eta()
+   */
+   template <class OtherLorentzVector>
+   Scalar DeltaR(const OtherLorentzVector &v, const bool useRapidity = false) const
+   {
+      const double delta = useRapidity ? Rapidity() - v.Rapidity() : Eta() - v.Eta();
+      double dphi = Phi() - v.Phi();
+      // convert dphi angle to the interval (-PI,PI]
+      if (dphi > TMath::Pi() || dphi <= -TMath::Pi()) {
+         if (dphi > 0) {
+            int n = static_cast<int>(dphi / TMath::TwoPi() + 0.5);
+            dphi -= TMath::TwoPi() * n;
+         } else {
+            int n = static_cast<int>(0.5 - dphi / TMath::TwoPi());
+            dphi += TMath::TwoPi() * n;
+         }
+      }
+      return math_sqrt(delta * delta + dphi * dphi);
+   }
 
    /**
       get the spatial components of the Vector in a

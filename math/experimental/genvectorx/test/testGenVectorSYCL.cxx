79920477b4b1546c9aa6f8a638f4c20797c8729f
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-09-10 15:03:05 +0200

    fixup! [backport][math] implement genvector custom axis rotation

--- math/experimental/genvectorx/test/testGenVectorSYCL.cxx
+++ math/experimental/genvectorx/test/testGenVectorSYCL.cxx
@@ -510,9 +510,9 @@ int testRotations3D()
             // at.Rotate(angle, axist);
             // at.Print();
             // (17.856456,8.106555,-21.199782)
-            iret |= compare(rotated.X(), 17.856456, 1e10);
-            iret |= compare(rotated.Y(), 8.106555, 1e10);
-            iret |= compare(rotated.Z(), -21.199782, 1e10);
+            iret[0] |= compare(rotated.X(), 17.856456, 1e10);
+            iret[0] |= compare(rotated.Y(), 8.106555, 1e10);
+            iret[0] |= compare(rotated.Z(), -21.199782, 1e10);
          });
       });
    }

16558d3dd866615978a36cfa3a3bacc38c2a82b0
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-10-01 18:40:40 +0200

    [cling] Require C++20 for SourceLocation test

--- interpreter/cling/test/Prompt/ValuePrinter/SourceLocation.C
+++ interpreter/cling/test/Prompt/ValuePrinter/SourceLocation.C
@@ -6,24 +6,18 @@
 // LICENSE.TXT for details.
 
 //------------------------------------------------------------------------------
-// RUN: cat %s | %cling | FileCheck %s
+// This test printing of std::source_location, which is a C++20 feature.
+// Running it with an older language standard is meaningless and brittle to test
+// Explicitly enforce -std=c++20 here.
+// RUN: cat %s | %cling -std=c++20 | FileCheck %s
 
 #include <iostream>
-#if __cplusplus >= 202002L
-#include <version>
-#endif
 
-#ifndef __cpp_lib_source_location
-// Hack to prevent failure if __cpp_lib_source_location feature does not exist!
-std::cout << "(std::source_location) ";
-std::cout << "CHECK_SRCLOC:42:std::source_location getsrcloc()\n";
-#else
 #include <source_location>
 std::source_location getsrcloc() {
 #line 42 "CHECK_SRCLOC"
   return std::source_location::current();
 }
 getsrcloc()
-#endif
 // CHECK: (std::source_location)
 // CHECK: CHECK_SRCLOC:42:std::source_location getsrcloc()

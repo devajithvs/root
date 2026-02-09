devajithvs/root

commit 9cdffe0c4435963df13b13df115b79f96c4d6b5d

[cling] Replace all occurrences of `TextInput` with `LineEditor`
- Update tests as errors/warnings will add a new line in TextInput
- Add `CLING_HISTSIZE` functionality with the newly added LLVM patch.
- Update `cling_history` test to match encoded libedit history

interpreter/cling/lib/UserInterface/UserInterface.cpp
@@ -12,13 +12,9 @@
 #include "cling/Interpreter/Exception.h"
 #include "cling/MetaProcessor/MetaProcessor.h"
 #include "cling/Utils/Output.h"
-#include "textinput/Callbacks.h"
-#include "textinput/History.h"
-#include "textinput/StreamReader.h"
-#include "textinput/TerminalDisplay.h"
-#include "textinput/TextInput.h"
 
 #include "llvm/ADT/SmallString.h"
+#include "llvm/LineEditor/LineEditor.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/Path.h"
 
@@ -30,43 +26,39 @@ namespace {
   /// to code complete through its own textinput mechanism which is part of the
   /// UserInterface.
   ///
-  class UITabCompletion : public textinput::TabCompletion {
-    const cling::Interpreter& m_ParentInterpreter;
 
-  public:
-    UITabCompletion(const cling::Interpreter& Parent) :
-                    m_ParentInterpreter(Parent) {}
-    ~UITabCompletion() {}
+  struct ClingListCompleter {
+    const cling::Interpreter& MainInterp;
 
-    bool Complete(textinput::Text& Line /*in+out*/,
-                  size_t& Cursor /*in+out*/,
-                  textinput::EditorRange& R /*out*/,
-                  std::vector<std::string>& Completions /*out*/) override {
-      m_ParentInterpreter.codeComplete(Line.GetText(), Cursor, Completions);
-      return true;
-    }
+    ClingListCompleter(const cling::Interpreter& Interp) : MainInterp(Interp) {}
+
+    std::vector<llvm::LineEditor::Completion> operator()(llvm::StringRef Buffer,
+                                                         size_t Pos) const;
+    std::vector<llvm::LineEditor::Completion>
+    operator()(llvm::StringRef Buffer, size_t Pos, llvm::Error& ErrRes) const;
   };
 
-  ///\brief Delays ~TextInput until after ~StreamReader and ~TerminalDisplay
-  ///
-  class TextInputHolder {
-    textinput::StreamReader* m_Reader;
-    textinput::TerminalDisplay* m_Display;
-    textinput::TextInput m_Input;
+  std::vector<llvm::LineEditor::Completion>
+  ClingListCompleter::operator()(llvm::StringRef Buffer, size_t Pos) const {
+    auto Err = llvm::Error::success();
+    auto res = (*this)(Buffer, Pos, Err);
+    if (Err)
+      llvm::logAllUnhandledErrors(std::move(Err), llvm::errs(), "error: ");
+    return res;
+  }
 
-  public:
-    TextInputHolder(llvm::SmallString<512>& Hist)
-        : m_Reader(textinput::StreamReader::Create()),
-          m_Display(textinput::TerminalDisplay::Create()),
-          m_Input(*m_Reader, *m_Display, Hist.empty() ? 0 : Hist.c_str()) {}
+  std::vector<llvm::LineEditor::Completion>
+  ClingListCompleter::operator()(llvm::StringRef Buffer, size_t Pos,
+                                 llvm::Error& ErrRes) const {
+    std::vector<llvm::LineEditor::Completion> Comps;
+    std::vector<std::string> Results;
 
-    ~TextInputHolder() {
-      delete m_Reader;
-      delete m_Display;
+    MainInterp.codeComplete(Buffer.str(), Pos, Results);
+    for (auto c : Results) {
+      Comps.push_back(llvm::LineEditor::Completion(c, c));
     }
-
-    textinput::TextInput* operator -> () { return &m_Input; }
-  };
+    return Comps;
+  }
 
   llvm::SmallString<512> GetHistoryFilePath() {
     if (std::getenv("CLING_NOHISTORY")) {
@@ -123,42 +115,27 @@ namespace cling {
     }
 
     auto histfilePath{GetHistoryFilePath()};
-
-    const auto Completion =
-        std::make_unique<UITabCompletion>(m_MetaProcessor->getInterpreter());
-    TextInputHolder TI(histfilePath);
-
-    TI->SetCompletion(Completion.get());
+    llvm::LineEditor LE("cling", histfilePath);
+    std::string Input;
+    std::string Prompt("[cling]$ ");
 
     if (const char* HistSizeEnvvar = std::getenv("CLING_HISTSIZE")) {
       const size_t HistSize = std::strtoull(HistSizeEnvvar, nullptr, 0);
 
       // std::strtoull() returns 0 if the parsing fails.
       // zero HistSize will disable history logging to file.
-      // refer to textinput::History::AppendToFile()
-      TI->SetHistoryMaxDepth(HistSize);
-      TI->SetHistoryPruneLength(
-          static_cast<size_t>(textinput::History::kPruneLengthDefault));
+      LE.setHistorySize(HistSize);
     }
 
-    bool Done = false;
-    std::string Line;
-    std::string Prompt("[cling]$ ");
+    LE.setPrompt(Prompt);
 
-    while (!Done) {
+    LE.setListCompleter(ClingListCompleter(m_MetaProcessor->getInterpreter()));
+    while (std::optional<std::string> Line = LE.readLine()) {
       try {
-        m_MetaProcessor->getOuts().flush();
-        {
-          MetaProcessor::MaybeRedirectOutputRAII RAII(*m_MetaProcessor);
-          TI->SetPrompt(Prompt.c_str());
-          Done = TI->ReadInput() == textinput::TextInput::kRREOF;
-          TI->TakeInput(Line);
-          if (Done && Line.empty())
-            break;
-        }
+        m_MetaProcessor->getOuts().flush(); // Do we need this now?
 
         cling::Interpreter::CompilationResult compRes;
-        const int indent = m_MetaProcessor->process(Line, compRes);
+        const int indent = m_MetaProcessor->process(*Line, compRes);
 
         // Quit requested?
         if (indent < 0)
@@ -186,8 +163,10 @@ namespace cling {
       catch(...) {
         cling::errs() << "Exception occurred. Recovering...\n";
       }
+
+      LE.setPrompt(Prompt);
     }
-    m_MetaProcessor->getOuts().flush();
+    m_MetaProcessor->getOuts().flush(); // Do we need this now?
   }
 
   void UserInterface::PrintLogo() {

interpreter/cling/lib/CMakeLists.txt
@@ -8,8 +8,7 @@
 
 add_subdirectory(Interpreter)
 add_subdirectory(MetaProcessor)
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/UserInterface/textinput OR
-   CLING_INCLUDE_TESTS)
+if(CLING_INCLUDE_TESTS)
    add_subdirectory(UserInterface)
 endif()
 add_subdirectory(Utils)

interpreter/cling/lib/UserInterface/CMakeLists.txt
@@ -7,40 +7,12 @@
 #------------------------------------------------------------------------------
 
 set( LLVM_LINK_COMPONENTS
+  LineEditor
   support
 )
 
-if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/textinput)
-  set(TEXTINPUTSRC ${CMAKE_SOURCE_DIR}/core/textinput/src/)
-  include_directories(${TEXTINPUTSRC})
-else()
-  # For cling, install textinput *.h in include/cling/UserInterface/textinput.
-  install(DIRECTORY ${TEXTINPUTSRC}textinput
-    DESTINATION include/cling/UserInterface
-    FILES_MATCHING
-    PATTERN "CMakeFiles" EXCLUDE
-    PATTERN "*.cpp" EXCLUDE
-    PATTERN "doc" EXCLUDE
-    PATTERN "*.h"
-    )
-endif()
-
 add_cling_library(clingUserInterface
   UserInterface.cpp
-  ${TEXTINPUTSRC}textinput/Editor.cpp
-  ${TEXTINPUTSRC}textinput/History.cpp
-  ${TEXTINPUTSRC}textinput/KeyBinding.cpp
-  ${TEXTINPUTSRC}textinput/Range.cpp
-  ${TEXTINPUTSRC}textinput/SignalHandler.cpp
-  ${TEXTINPUTSRC}textinput/StreamReader.cpp
-  ${TEXTINPUTSRC}textinput/StreamReaderUnix.cpp
-  ${TEXTINPUTSRC}textinput/StreamReaderWin.cpp
-  ${TEXTINPUTSRC}textinput/TerminalConfigUnix.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplay.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplayUnix.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplayWin.cpp
-  ${TEXTINPUTSRC}textinput/TextInput.cpp
-  ${TEXTINPUTSRC}textinput/TextInputContext.cpp
 
   LINK_LIBS
   clingMetaProcessor

interpreter/cling/test/CodeGeneration/Statics.C
@@ -37,5 +37,5 @@ inst02();
 // expected-no-diagnostics
 .q
 
-// CHECK-NEXT: TERD::~TERD::inst02
+// CHECK: TERD::~TERD::inst02
 // CHECK-NEXT: TERD::~TERD::inst01

interpreter/cling/test/CodeGeneration/const.C
@@ -33,5 +33,5 @@ a.val
 a.getVal()
 // CHECK-NEXT: (int) 1
 
-// CHECK-NEXT: ~A(1), this = [[PTR]]
+// CHECK: ~A(1), this = [[PTR]]
 // CHECK-NOT: ~A

interpreter/cling/test/CodeUnloading/AtExit.C
@@ -115,7 +115,7 @@ struct ShutdownRecursion {
 
 // Reversed registration order
 
-// CHECK-NEXT: ~ShutdownRecursion
+// CHECK: ~ShutdownRecursion
 // CHECK-NEXT: ShutdownRecursion2
 // CHECK-NEXT: ShutdownRecursion1
 // CHECK-NEXT: ShutdownRecursion0

interpreter/cling/test/Driver/Inputs/cling_history
@@ -1,8 +1,9 @@
-/*
-comment
- * */
-static const int i = 37;
-};
-// another comment
-using std::cout;
-std::cout << "test\n";
+_HiStOrY_V2_
+/*\012
+\011\011comment\012
+\040*\040*/\012
+\011static\040const\040int\040i\040=\04037;\012
+};\012
+//\040another\040comment\012
+using\040std::cout;\012
+std::cout\040<<\040"test\134n";\012

interpreter/cling/test/Interfaces/evaluate.C
@@ -224,7 +224,8 @@ gCling->evaluate("arrV", V);
 
 // Destruct the variables with static storage:
 // Destruct arrV:
-//CHECK-NEXT: MADE{7}:dtor
+// The error `err_verify_no_directives` will add a new line here.
+//CHECK: MADE{7}:dtor
 //CHECK-NEXT: MADE{6}:dtor
 //CHECK-NEXT: MADE{5}:dtor
 

commit 91848407abda75fe4451f9e5e68d92cc2701706d

[cling] Fix broken autocomplete
This also moves closer to clang-repl autocomplete machinery:

`ClingCodeCompleteConsumer` is now almost identical to
`clang::ReplCodeCompleter`

Autocomplete in `cling` was broken after 9cdffe0 since cling does
autocomplete a bit differently from `clang-repl`

interpreter/cling/include/cling/Interpreter/ClingCodeCompleteConsumer.h
@@ -5,7 +5,13 @@
 // This file is dual-licensed: you can choose to license it under the University
 // of Illinois Open Source License or the GNU Lesser General Public License. See
 // LICENSE.TXT for details.
-//------------------------------------------------------------------------------
+//
+//===----------------------------------------------------------------------===//
+//
+// This file follows the same structure/logic (duplicate) as:
+// clang/Interpreter/CodeCompletion.h
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef CLING_CODE_COMPLETE_CONSUMER
 #define CLING_CODE_COMPLETE_CONSUMER
@@ -19,42 +25,6 @@ namespace clang {
 }
 
 namespace cling {
-  /// \brief Create a new printing code-completion consumer that prints its
-  /// results to the given raw output stream.
-  class ClingCodeCompleteConsumer : public CodeCompleteConsumer {
-    CodeCompletionTUInfo m_CCTUInfo;
-    /// \ brief Results of the completer to be printed by the text interface.
-    std::vector<std::string> &m_Completions;
-
-  public:
-    ClingCodeCompleteConsumer(const CodeCompleteOptions &CodeComplOpts,
-                              std::vector<std::string> &completions)
-      : CodeCompleteConsumer(CodeComplOpts),
-        m_CCTUInfo(std::make_shared<GlobalCodeCompletionAllocator>()),
-        m_Completions(completions) {}
-    ~ClingCodeCompleteConsumer() {}
-
-    /// \brief Prints the finalized code-completion results.
-    void ProcessCodeCompleteResults(Sema &S, CodeCompletionContext Context,
-                                    CodeCompletionResult *Results,
-                                    unsigned NumResults) override;
-
-    CodeCompletionAllocator &getAllocator() override {
-      return m_CCTUInfo.getAllocator();
-    }
-
-    CodeCompletionTUInfo &getCodeCompletionTUInfo() override {
-      return m_CCTUInfo;
-    }
-
-    bool isResultFilteredOut(StringRef Filter,
-                             CodeCompletionResult Results) override;
-
-    void getCompletions(std::vector<std::string>& completions) {
-      completions = m_Completions;
-    }
-  };
-
   struct ClingCodeCompleter {
     ClingCodeCompleter() = default;
     std::string Prefix;
@@ -67,10 +37,11 @@ namespace cling {
     /// \param[in] ParentCI The running interpreter compiler instance that
     ///                     provides ASTContexts.
     /// \param[out] CCResults The completion results.
-    void codeComplete(CompilerInstance* InterpCI, llvm::StringRef Content,
-                      unsigned Line, unsigned Col, CompilerInstance* ParentCI,
+    void codeComplete(clang::CompilerInstance* InterpCI,
+                      llvm::StringRef Content, unsigned Line, unsigned Col,
+                      clang::CompilerInstance* ParentCI,
                       std::vector<std::string>& CCResults);
   };
-}
+} // namespace cling
 
 #endif

interpreter/cling/lib/Interpreter/ClingCodeCompleteConsumer.cpp
@@ -5,83 +5,207 @@
 // This file is dual-licensed: you can choose to license it under the University
 // of Illinois Open Source License or the GNU Lesser General Public License. See
 // LICENSE.TXT for details.
-//------------------------------------------------------------------------------
+//
+//===----------------------------------------------------------------------===//
+//
+// This is mostly a copy of clang/lib/Interpreter/CodeCompletion.cpp
+// TODO: Replace with ReplCodeCompleter
+//
+//===----------------------------------------------------------------------===//
 
 #include "cling/Interpreter/ClingCodeCompleteConsumer.h"
 
 #include "clang/AST/ASTImporter.h"
 #include "clang/AST/DeclLookups.h"
-#include "clang/Basic/Diagnostic.h"
+#include "clang/AST/DeclarationName.h"
+#include "clang/AST/ExternalASTSource.h"
+#include "clang/Basic/IdentifierTable.h"
 #include "clang/Frontend/ASTUnit.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendActions.h"
-#include "clang/Lex/Preprocessor.h"
+#include "clang/Interpreter/Interpreter.h"
+#include "clang/Lex/PreprocessorOptions.h"
+#include "clang/Sema/CodeCompleteConsumer.h"
+#include "clang/Sema/CodeCompleteOptions.h"
 #include "clang/Sema/Sema.h"
 
 namespace cling {
-  void ClingCodeCompleteConsumer::ProcessCodeCompleteResults(Sema &SemaRef,
-                                                   CodeCompletionContext Context,
-                                                   CodeCompletionResult *Results,
-                                                           unsigned NumResults) {
-    std::stable_sort(Results, Results + NumResults);
+  const std::string CodeCompletionFileName = "input_line_[Completion]";
 
-    StringRef Filter = SemaRef.getPreprocessor().getCodeCompletionFilter();
-
-    for (unsigned I = 0; I != NumResults; ++I) {
-      if (!Filter.empty() && isResultFilteredOut(Filter, Results[I]))
-        continue;
-      switch (Results[I].Kind) {
-        case CodeCompletionResult::RK_Declaration:
-          if (CodeCompletionString *CCS
-              = Results[I].CreateCodeCompletionString(SemaRef, Context,
-                                                      getAllocator(),
-                                                      m_CCTUInfo,
-                                                      includeBriefComments())) {
-            m_Completions.push_back(CCS->getAsString());
-          }
-          break;
-
-        case CodeCompletionResult::RK_Keyword:
-          m_Completions.push_back(Results[I].Keyword);
-          break;
-
-        case CodeCompletionResult::RK_Macro:
-          if (CodeCompletionString *CCS
-              = Results[I].CreateCodeCompletionString(SemaRef, Context,
-                                                      getAllocator(),
-                                                      m_CCTUInfo,
-                                                      includeBriefComments())) {
-            m_Completions.push_back(CCS->getAsString());
-          }
-          break;
-
-        case CodeCompletionResult::RK_Pattern:
-          m_Completions.push_back(Results[I].Pattern->getAsString());
-          break;
-      }
-    }
+  clang::CodeCompleteOptions getClangCompleteOpts() {
+    clang::CodeCompleteOptions Opts;
+    Opts.IncludeCodePatterns = true;
+    Opts.IncludeMacros = true;
+    Opts.IncludeGlobals = true;
+    Opts.IncludeBriefComments = true;
+    return Opts;
   }
 
-  bool ClingCodeCompleteConsumer::isResultFilteredOut(StringRef Filter,
-                                                  CodeCompletionResult Result) {
-    switch (Result.Kind) {
-      case CodeCompletionResult::RK_Declaration: {
-        return !(
-            Result.Declaration->getIdentifier() &&
-            Result.Declaration->getIdentifier()->getName().starts_with(Filter));
-      }
-      case CodeCompletionResult::RK_Keyword: {
-        return !((StringRef(Result.Keyword)).starts_with(Filter));
-      }
-      case CodeCompletionResult::RK_Macro: {
-        return !(Result.Macro->getName().starts_with(Filter));
-      }
-      case CodeCompletionResult::RK_Pattern: {
-        return !(
-            StringRef((Result.Pattern->getAsString())).starts_with(Filter));
-      }
-      default: llvm_unreachable("Unknown code completion result Kind.");
+  /// Create a new printing code-completion consumer that prints its
+  /// results to the given raw output stream.
+  class ClingCompletionConsumer : public clang::CodeCompleteConsumer {
+  public:
+    ClingCompletionConsumer(std::vector<std::string>& Results,
+                            ClingCodeCompleter& CC)
+        : CodeCompleteConsumer(getClangCompleteOpts()),
+          CCAllocator(std::make_shared<GlobalCodeCompletionAllocator>()),
+          CCTUInfo(CCAllocator), Results(Results), CC(CC) {}
+
+    // The entry of handling code completion. When the function is called, we
+    // create a `Context`-based handler (see classes defined below) to handle
+    // each completion result.
+    void ProcessCodeCompleteResults(class Sema& S,
+                                    CodeCompletionContext Context,
+                                    CodeCompletionResult* InResults,
+                                    unsigned NumResults) final;
+
+    CodeCompletionAllocator& getAllocator() override { return *CCAllocator; }
+
+    CodeCompletionTUInfo& getCodeCompletionTUInfo() override {
+      return CCTUInfo;
     }
+
+  private:
+    std::shared_ptr<GlobalCodeCompletionAllocator> CCAllocator;
+    CodeCompletionTUInfo CCTUInfo;
+    std::vector<std::string>& Results;
+    ClingCodeCompleter& CC;
+  };
+
+  /// The class CompletionContextHandler contains four interfaces, each of
+  /// which handles one type of completion result.
+  /// Its derived classes are used to create concrete handlers based on
+  /// \c CodeCompletionContext.
+  class CompletionContextHandler {
+  protected:
+    CodeCompletionContext CCC;
+    std::vector<std::string>& Results;
+
+  private:
+    Sema& S;
+
+  public:
+    CompletionContextHandler(Sema& S, CodeCompletionContext CCC,
+                             std::vector<std::string>& Results)
+        : CCC(CCC), Results(Results), S(S) {}
+
+    virtual ~CompletionContextHandler() = default;
+    /// Converts a Declaration completion result to a completion string, and
+    /// then stores it in Results.
+    virtual void handleDeclaration(const CodeCompletionResult& Result) {
+      auto PreferredType = CCC.getPreferredType();
+      if (PreferredType.isNull()) {
+        Results.push_back(Result.Declaration->getName().str());
+        return;
+      }
+
+      if (auto* VD = dyn_cast<VarDecl>(Result.Declaration)) {
+        auto ArgumentType = VD->getType();
+        if (PreferredType->isReferenceType()) {
+          QualType RT =
+              PreferredType->castAs<ReferenceType>()->getPointeeType();
+          Sema::ReferenceConversions RefConv;
+          Sema::ReferenceCompareResult RefRelationship =
+              S.CompareReferenceRelationship(SourceLocation(), RT, ArgumentType,
+                                             &RefConv);
+          switch (RefRelationship) {
+            case Sema::Ref_Compatible:
+            case Sema::Ref_Related:
+              Results.push_back(VD->getName().str());
+              break;
+            case Sema::Ref_Incompatible: break;
+          }
+        } else if (S.Context.hasSameType(ArgumentType, PreferredType)) {
+          Results.push_back(VD->getName().str());
+        }
+      }
+    }
+
+    /// Converts a Keyword completion result to a completion string, and then
+    /// stores it in Results.
+    virtual void handleKeyword(const CodeCompletionResult& Result) {
+      auto Prefix = S.getPreprocessor().getCodeCompletionFilter();
+      // Add keyword to the completion results only if we are in a type-aware
+      // situation.
+      if (!CCC.getBaseType().isNull() || !CCC.getPreferredType().isNull())
+        return;
+      if (StringRef(Result.Keyword).starts_with(Prefix))
+        Results.push_back(Result.Keyword);
+    }
+
+    /// Converts a Pattern completion result to a completion string, and then
+    /// stores it in Results.
+    virtual void handlePattern(const CodeCompletionResult& Result) {}
+
+    /// Converts a Macro completion result to a completion string, and then
+    /// stores it in Results.
+    virtual void handleMacro(const CodeCompletionResult& Result) {}
+  };
+
+  class DotMemberAccessHandler : public CompletionContextHandler {
+  public:
+    DotMemberAccessHandler(Sema& S, CodeCompletionContext CCC,
+                           std::vector<std::string>& Results)
+        : CompletionContextHandler(S, CCC, Results) {}
+    void handleDeclaration(const CodeCompletionResult& Result) override {
+      auto* ID = Result.Declaration->getIdentifier();
+      if (!ID)
+        return;
+      if (!isa<CXXMethodDecl>(Result.Declaration))
+        return;
+      const auto* Fun = cast<CXXMethodDecl>(Result.Declaration);
+      if (Fun->getParent()->getCanonicalDecl() ==
+          CCC.getBaseType()->getAsCXXRecordDecl()->getCanonicalDecl()) {
+        Results.push_back(ID->getName().str());
+      }
+    }
+
+    void handleKeyword(const CodeCompletionResult& Result) override {}
+  };
+
+  void ClingCompletionConsumer::ProcessCodeCompleteResults(
+      class Sema& S, CodeCompletionContext Context,
+      CodeCompletionResult* InResults, unsigned NumResults) {
+
+    auto Prefix = S.getPreprocessor().getCodeCompletionFilter();
+    CC.Prefix = Prefix;
+
+    std::unique_ptr<CompletionContextHandler> CCH;
+
+    // initialize fine-grained code completion handler based on the code
+    // completion context.
+    switch (Context.getKind()) {
+      case CodeCompletionContext::CCC_DotMemberAccess:
+        CCH.reset(new DotMemberAccessHandler(S, Context, this->Results));
+        break;
+      default:
+        CCH.reset(new CompletionContextHandler(S, Context, this->Results));
+    };
+
+    for (unsigned I = 0; I < NumResults; I++) {
+      auto& Result = InResults[I];
+      switch (Result.Kind) {
+        case CodeCompletionResult::RK_Declaration:
+          if (Result.Hidden) {
+            break;
+          }
+          if (!Result.Declaration->getDeclName().isIdentifier() ||
+              !Result.Declaration->getName().starts_with(Prefix)) {
+            break;
+          }
+          CCH->handleDeclaration(Result);
+          break;
+        case CodeCompletionResult::RK_Keyword:
+          CCH->handleKeyword(Result);
+          break;
+        case CodeCompletionResult::RK_Macro: CCH->handleMacro(Result); break;
+        case CodeCompletionResult::RK_Pattern:
+          CCH->handlePattern(Result);
+          break;
+      }
+    }
+
+    std::sort(Results.begin(), Results.end());
   }
 
   // Code copied from: clang/lib/Interpreter/CodeCompletion.cpp
@@ -235,23 +359,18 @@ namespace cling {
                                         std::vector<std::string>& CCResults) {
     const std::string CodeCompletionFileName = "input_line_[Completion]";
     auto DiagOpts = DiagnosticOptions();
-    auto consumer =
-        ClingCodeCompleteConsumer(ParentCI->getFrontendOpts().CodeCompleteOpts,
-                                  CCResults);
-    ;
+    auto consumer = ClingCompletionConsumer(CCResults, *this);
 
     auto diag = InterpCI->getDiagnosticsPtr();
-    std::unique_ptr<clang::ASTUnit> AU(
-        clang::ASTUnit::LoadFromCompilerInvocationAction(
-            InterpCI->getInvocationPtr(),
-            std::make_shared<PCHContainerOperations>(), diag));
+    std::unique_ptr<ASTUnit> AU(ASTUnit::LoadFromCompilerInvocationAction(
+        InterpCI->getInvocationPtr(),
+        std::make_shared<PCHContainerOperations>(), diag));
     llvm::SmallVector<clang::StoredDiagnostic, 8> sd = {};
     llvm::SmallVector<const llvm::MemoryBuffer*, 1> tb = {};
     InterpCI->getFrontendOpts().Inputs[0] =
         FrontendInputFile(CodeCompletionFileName, Language::CXX,
                           InputKind::Source);
-    auto Act = std::unique_ptr<IncrementalSyntaxOnlyAction>(
-        new IncrementalSyntaxOnlyAction(ParentCI));
+    auto Act = std::make_unique<IncrementalSyntaxOnlyAction>(ParentCI);
     std::unique_ptr<llvm::MemoryBuffer> MB =
         llvm::MemoryBuffer::getMemBufferCopy(Content, CodeCompletionFileName);
     llvm::SmallVector<ASTUnit::RemappedFile, 4> RemappedFiles;
@@ -263,7 +382,7 @@ namespace cling {
     AU->CodeComplete(CodeCompletionFileName, 1, Col, RemappedFiles, false,
                      false, false, consumer,
                      std::make_shared<clang::PCHContainerOperations>(), *diag,
-                     InterpCI->getLangOpts(), InterpCI->getSourceManager(),
-                     InterpCI->getFileManager(), sd, tb, std::move(Act));
+                     InterpCI->getLangOpts(), AU->getSourceManager(),
+                     AU->getFileManager(), sd, tb, std::move(Act));
   }
 }

interpreter/cling/lib/UserInterface/UserInterface.cpp
@@ -9,6 +9,8 @@
 
 #include "cling/UserInterface/UserInterface.h"
 
+#include "cling/Interpreter/CIFactory.h"
+#include "cling/Interpreter/ClingCodeCompleteConsumer.h"
 #include "cling/Interpreter/Exception.h"
 #include "cling/MetaProcessor/MetaProcessor.h"
 #include "cling/Utils/Output.h"
@@ -53,9 +55,30 @@ namespace {
     std::vector<llvm::LineEditor::Completion> Comps;
     std::vector<std::string> Results;
 
-    MainInterp.codeComplete(Buffer.str(), Pos, Results);
+    std::string resourceDir =
+        MainInterp.getCI()->getHeaderSearchOpts().ResourceDir;
+    // Remove the extra 3 directory names "/lib/clang/3.9.0"
+    llvm::StringRef parentResourceDir =
+        llvm::sys::path::parent_path(llvm::sys::path::parent_path(
+            llvm::sys::path::parent_path(resourceDir)));
+    std::string llvmDir = parentResourceDir.str();
+
+    // arguments for constructing CI
+    const cling::Interpreter::ModuleFileExtensions& moduleExtensions = {};
+
+    auto InterpCI = std::unique_ptr<clang::CompilerInstance>(
+        cling::CIFactory::createCI("\n", MainInterp.getOptions(),
+                                   llvmDir.c_str(), std::nullopt,
+                                   moduleExtensions,
+                                   /*AutoComplete=*/true));
+    auto CC = cling::ClingCodeCompleter();
+    CC.codeComplete(InterpCI.get(), Buffer.str(), 1U, Pos + 1,
+                    MainInterp.getCI(), Results);
+
     for (auto c : Results) {
-      Comps.push_back(llvm::LineEditor::Completion(c, c));
+      if (c.find(CC.Prefix) == 0)
+        Comps.push_back(
+            llvm::LineEditor::Completion(c.substr(CC.Prefix.size()), c));
     }
     return Comps;
   }
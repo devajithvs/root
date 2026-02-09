devajithvs/root

commit 9cdffe0c4435963df13b13df115b79f96c4d6b5d

[cling] Replace all occurrences of `TextInput` with `LineEditor`
- Update tests as errors/warnings will add a new line in TextInput
- Add `CLING_HISTSIZE` functionality with the newly added LLVM patch.
- Update `cling_history` test to match encoded libedit history

interpreter/cling/lib/UserInterface/UserInterface.cpp
@@ -12,13 +12,9 @@
 #include "cling/Interpreter/Exception.h"
 #include "cling/MetaProcessor/MetaProcessor.h"
 #include "cling/Utils/Output.h"
-#include "textinput/Callbacks.h"
-#include "textinput/History.h"
-#include "textinput/StreamReader.h"
-#include "textinput/TerminalDisplay.h"
-#include "textinput/TextInput.h"
 
 #include "llvm/ADT/SmallString.h"
+#include "llvm/LineEditor/LineEditor.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/Path.h"
 
@@ -30,43 +26,39 @@ namespace {
   /// to code complete through its own textinput mechanism which is part of the
   /// UserInterface.
   ///
-  class UITabCompletion : public textinput::TabCompletion {
-    const cling::Interpreter& m_ParentInterpreter;
 
-  public:
-    UITabCompletion(const cling::Interpreter& Parent) :
-                    m_ParentInterpreter(Parent) {}
-    ~UITabCompletion() {}
+  struct ClingListCompleter {
+    const cling::Interpreter& MainInterp;
 
-    bool Complete(textinput::Text& Line /*in+out*/,
-                  size_t& Cursor /*in+out*/,
-                  textinput::EditorRange& R /*out*/,
-                  std::vector<std::string>& Completions /*out*/) override {
-      m_ParentInterpreter.codeComplete(Line.GetText(), Cursor, Completions);
-      return true;
-    }
+    ClingListCompleter(const cling::Interpreter& Interp) : MainInterp(Interp) {}
+
+    std::vector<llvm::LineEditor::Completion> operator()(llvm::StringRef Buffer,
+                                                         size_t Pos) const;
+    std::vector<llvm::LineEditor::Completion>
+    operator()(llvm::StringRef Buffer, size_t Pos, llvm::Error& ErrRes) const;
   };
 
-  ///\brief Delays ~TextInput until after ~StreamReader and ~TerminalDisplay
-  ///
-  class TextInputHolder {
-    textinput::StreamReader* m_Reader;
-    textinput::TerminalDisplay* m_Display;
-    textinput::TextInput m_Input;
+  std::vector<llvm::LineEditor::Completion>
+  ClingListCompleter::operator()(llvm::StringRef Buffer, size_t Pos) const {
+    auto Err = llvm::Error::success();
+    auto res = (*this)(Buffer, Pos, Err);
+    if (Err)
+      llvm::logAllUnhandledErrors(std::move(Err), llvm::errs(), "error: ");
+    return res;
+  }
 
-  public:
-    TextInputHolder(llvm::SmallString<512>& Hist)
-        : m_Reader(textinput::StreamReader::Create()),
-          m_Display(textinput::TerminalDisplay::Create()),
-          m_Input(*m_Reader, *m_Display, Hist.empty() ? 0 : Hist.c_str()) {}
+  std::vector<llvm::LineEditor::Completion>
+  ClingListCompleter::operator()(llvm::StringRef Buffer, size_t Pos,
+                                 llvm::Error& ErrRes) const {
+    std::vector<llvm::LineEditor::Completion> Comps;
+    std::vector<std::string> Results;
 
-    ~TextInputHolder() {
-      delete m_Reader;
-      delete m_Display;
+    MainInterp.codeComplete(Buffer.str(), Pos, Results);
+    for (auto c : Results) {
+      Comps.push_back(llvm::LineEditor::Completion(c, c));
     }
-
-    textinput::TextInput* operator -> () { return &m_Input; }
-  };
+    return Comps;
+  }
 
   llvm::SmallString<512> GetHistoryFilePath() {
     if (std::getenv("CLING_NOHISTORY")) {
@@ -123,42 +115,27 @@ namespace cling {
     }
 
     auto histfilePath{GetHistoryFilePath()};
-
-    const auto Completion =
-        std::make_unique<UITabCompletion>(m_MetaProcessor->getInterpreter());
-    TextInputHolder TI(histfilePath);
-
-    TI->SetCompletion(Completion.get());
+    llvm::LineEditor LE("cling", histfilePath);
+    std::string Input;
+    std::string Prompt("[cling]$ ");
 
     if (const char* HistSizeEnvvar = std::getenv("CLING_HISTSIZE")) {
       const size_t HistSize = std::strtoull(HistSizeEnvvar, nullptr, 0);
 
       // std::strtoull() returns 0 if the parsing fails.
       // zero HistSize will disable history logging to file.
-      // refer to textinput::History::AppendToFile()
-      TI->SetHistoryMaxDepth(HistSize);
-      TI->SetHistoryPruneLength(
-          static_cast<size_t>(textinput::History::kPruneLengthDefault));
+      LE.setHistorySize(HistSize);
     }
 
-    bool Done = false;
-    std::string Line;
-    std::string Prompt("[cling]$ ");
+    LE.setPrompt(Prompt);
 
-    while (!Done) {
+    LE.setListCompleter(ClingListCompleter(m_MetaProcessor->getInterpreter()));
+    while (std::optional<std::string> Line = LE.readLine()) {
       try {
-        m_MetaProcessor->getOuts().flush();
-        {
-          MetaProcessor::MaybeRedirectOutputRAII RAII(*m_MetaProcessor);
-          TI->SetPrompt(Prompt.c_str());
-          Done = TI->ReadInput() == textinput::TextInput::kRREOF;
-          TI->TakeInput(Line);
-          if (Done && Line.empty())
-            break;
-        }
+        m_MetaProcessor->getOuts().flush(); // Do we need this now?
 
         cling::Interpreter::CompilationResult compRes;
-        const int indent = m_MetaProcessor->process(Line, compRes);
+        const int indent = m_MetaProcessor->process(*Line, compRes);
 
         // Quit requested?
         if (indent < 0)
@@ -186,8 +163,10 @@ namespace cling {
       catch(...) {
         cling::errs() << "Exception occurred. Recovering...\n";
       }
+
+      LE.setPrompt(Prompt);
     }
-    m_MetaProcessor->getOuts().flush();
+    m_MetaProcessor->getOuts().flush(); // Do we need this now?
   }
 
   void UserInterface::PrintLogo() {

interpreter/cling/lib/CMakeLists.txt
@@ -8,8 +8,7 @@
 
 add_subdirectory(Interpreter)
 add_subdirectory(MetaProcessor)
-if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/UserInterface/textinput OR
-   CLING_INCLUDE_TESTS)
+if(CLING_INCLUDE_TESTS)
    add_subdirectory(UserInterface)
 endif()
 add_subdirectory(Utils)

interpreter/cling/lib/UserInterface/CMakeLists.txt
@@ -7,40 +7,12 @@
 #------------------------------------------------------------------------------
 
 set( LLVM_LINK_COMPONENTS
+  LineEditor
   support
 )
 
-if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/textinput)
-  set(TEXTINPUTSRC ${CMAKE_SOURCE_DIR}/core/textinput/src/)
-  include_directories(${TEXTINPUTSRC})
-else()
-  # For cling, install textinput *.h in include/cling/UserInterface/textinput.
-  install(DIRECTORY ${TEXTINPUTSRC}textinput
-    DESTINATION include/cling/UserInterface
-    FILES_MATCHING
-    PATTERN "CMakeFiles" EXCLUDE
-    PATTERN "*.cpp" EXCLUDE
-    PATTERN "doc" EXCLUDE
-    PATTERN "*.h"
-    )
-endif()
-
 add_cling_library(clingUserInterface
   UserInterface.cpp
-  ${TEXTINPUTSRC}textinput/Editor.cpp
-  ${TEXTINPUTSRC}textinput/History.cpp
-  ${TEXTINPUTSRC}textinput/KeyBinding.cpp
-  ${TEXTINPUTSRC}textinput/Range.cpp
-  ${TEXTINPUTSRC}textinput/SignalHandler.cpp
-  ${TEXTINPUTSRC}textinput/StreamReader.cpp
-  ${TEXTINPUTSRC}textinput/StreamReaderUnix.cpp
-  ${TEXTINPUTSRC}textinput/StreamReaderWin.cpp
-  ${TEXTINPUTSRC}textinput/TerminalConfigUnix.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplay.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplayUnix.cpp
-  ${TEXTINPUTSRC}textinput/TerminalDisplayWin.cpp
-  ${TEXTINPUTSRC}textinput/TextInput.cpp
-  ${TEXTINPUTSRC}textinput/TextInputContext.cpp
 
   LINK_LIBS
   clingMetaProcessor

interpreter/cling/test/CodeGeneration/Statics.C
@@ -37,5 +37,5 @@ inst02();
 // expected-no-diagnostics
 .q
 
-// CHECK-NEXT: TERD::~TERD::inst02
+// CHECK: TERD::~TERD::inst02
 // CHECK-NEXT: TERD::~TERD::inst01

interpreter/cling/test/CodeGeneration/const.C
@@ -33,5 +33,5 @@ a.val
 a.getVal()
 // CHECK-NEXT: (int) 1
 
-// CHECK-NEXT: ~A(1), this = [[PTR]]
+// CHECK: ~A(1), this = [[PTR]]
 // CHECK-NOT: ~A

interpreter/cling/test/CodeUnloading/AtExit.C
@@ -115,7 +115,7 @@ struct ShutdownRecursion {
 
 // Reversed registration order
 
-// CHECK-NEXT: ~ShutdownRecursion
+// CHECK: ~ShutdownRecursion
 // CHECK-NEXT: ShutdownRecursion2
 // CHECK-NEXT: ShutdownRecursion1
 // CHECK-NEXT: ShutdownRecursion0

interpreter/cling/test/Driver/Inputs/cling_history
@@ -1,8 +1,9 @@
-/*
-comment
- * */
-static const int i = 37;
-};
-// another comment
-using std::cout;
-std::cout << "test\n";
+_HiStOrY_V2_
+/*\012
+\011\011comment\012
+\040*\040*/\012
+\011static\040const\040int\040i\040=\04037;\012
+};\012
+//\040another\040comment\012
+using\040std::cout;\012
+std::cout\040<<\040"test\134n";\012

interpreter/cling/test/Interfaces/evaluate.C
@@ -224,7 +224,8 @@ gCling->evaluate("arrV", V);
 
 // Destruct the variables with static storage:
 // Destruct arrV:
-//CHECK-NEXT: MADE{7}:dtor
+// The error `err_verify_no_directives` will add a new line here.
+//CHECK: MADE{7}:dtor
 //CHECK-NEXT: MADE{6}:dtor
 //CHECK-NEXT: MADE{5}:dtor
 

commit 379da668aa7005537375113b07bd5b0036103b2a

Fix autocomplete in cling
Autocomplete in cling was broken after https://github.com/root-project/root/commit/9cdffe0c4435963df13b13df115b79f96c4d6b5d since cling did autocomplete a bit differently from clang-repl

core/dictgen/src/LinkdefReader.cxx
@@ -1058,9 +1058,9 @@ bool LinkdefReader::Parse(SelectionRules &sr, llvm::StringRef code, const std::v
 
    // Extract all #pragmas
    std::unique_ptr<llvm::MemoryBuffer> memBuf = llvm::MemoryBuffer::getMemBuffer(code, "CLING #pragma extraction");
-   clang::CompilerInstance *pragmaCI = cling::CIFactory::createCI(std::move(memBuf), parserArgsC.size(),
-                                                                  &parserArgsC[0], llvmdir, nullptr /*Consumer*/,
-                                                                  {} /*ModuleFileExtension*/, true /*OnlyLex*/);
+   clang::CompilerInstance *pragmaCI =
+      cling::CIFactory::createCI(std::move(memBuf), parserArgsC.size(), &parserArgsC[0], llvmdir,
+                                 std::nullopt /*Consumer*/, {} /*ModuleFileExtension*/, true /*OnlyLex*/);
 
    clang::Preprocessor &PP = pragmaCI->getPreprocessor();
    clang::DiagnosticConsumer &DClient = pragmaCI->getDiagnosticClient();

interpreter/cling/include/cling/Interpreter/CIFactory.h
@@ -37,13 +37,15 @@ namespace cling {
 
     clang::CompilerInstance*
     createCI(llvm::StringRef Code, const InvocationOptions& Opts,
-             const char* LLVMDir, std::unique_ptr<clang::ASTConsumer> consumer,
+             const char* LLVMDir,
+             std::optional<std::unique_ptr<clang::ASTConsumer>> consumerOpt,
              const ModuleFileExtensions& moduleExtensions,
              bool AutoComplete = false);
 
     clang::CompilerInstance*
     createCI(MemBufPtr_t Buffer, int Argc, const char* const* Argv,
-             const char* LLVMDir, std::unique_ptr<clang::ASTConsumer> consumer,
+             const char* LLVMDir,
+             std::optional<std::unique_ptr<clang::ASTConsumer>> consumerOpt,
              const ModuleFileExtensions& moduleExtensions,
              bool OnlyLex = false);
   } // namespace CIFactory

interpreter/cling/lib/Interpreter/CIFactory.cpp
@@ -1700,7 +1700,7 @@ namespace {
 
     std::vector<std::unique_ptr<ASTConsumer>> Consumers;
 
-    if (!OnlyLex) {
+    if (!OnlyLex && !AutoComplete) {
       assert(customConsumer && "Need to specify a custom consumer"
                                " when not in OnlyLex mode");
       Consumers.push_back(std::move(customConsumer));
@@ -1834,25 +1834,27 @@ namespace {
 
 namespace cling {
 
-  CompilerInstance*
-  CIFactory::createCI(llvm::StringRef Code, const InvocationOptions& Opts,
-                      const char* LLVMDir,
-                      std::unique_ptr<clang::ASTConsumer> consumer,
-                      const ModuleFileExtensions& moduleExtensions,
-                      bool AutoComplete /*false*/) {
+  CompilerInstance* CIFactory::createCI(
+      llvm::StringRef Code, const InvocationOptions& Opts, const char* LLVMDir,
+      std::optional<std::unique_ptr<clang::ASTConsumer>> consumerOpt,
+      const ModuleFileExtensions& moduleExtensions,
+      bool AutoComplete /*false*/) {
     return createCIImpl(llvm::MemoryBuffer::getMemBuffer(Code),
-                        Opts.CompilerOpts, LLVMDir, std::move(consumer),
+                        Opts.CompilerOpts, LLVMDir,
+                        consumerOpt ? std::move(*consumerOpt) : nullptr,
                         moduleExtensions, false /*OnlyLex*/,
                         !Opts.IsInteractive(), AutoComplete);
   }
 
-CompilerInstance* CIFactory::createCI(
-    MemBufPtr_t Buffer, int argc, const char* const* argv, const char* LLVMDir,
-    std::unique_ptr<clang::ASTConsumer> consumer,
-    const ModuleFileExtensions& moduleExtensions, bool OnlyLex /*false*/) {
-  return createCIImpl(std::move(Buffer), CompilerOptions(argc, argv),  LLVMDir,
-                      std::move(consumer), moduleExtensions, OnlyLex);
-}
+  CompilerInstance* CIFactory::createCI(
+      MemBufPtr_t Buffer, int argc, const char* const* argv,
+      const char* LLVMDir,
+      std::optional<std::unique_ptr<clang::ASTConsumer>> consumerOpt,
+      const ModuleFileExtensions& moduleExtensions, bool OnlyLex /*false*/) {
+    return createCIImpl(std::move(Buffer), CompilerOptions(argc, argv), LLVMDir,
+                        consumerOpt ? std::move(*consumerOpt) : nullptr,
+                        moduleExtensions, OnlyLex);
+  }
 
 } // namespace cling
 

interpreter/cling/lib/Interpreter/IncrementalParser.cpp
@@ -293,7 +293,8 @@ namespace cling {
     std::unique_ptr<cling::DeclCollector> consumer;
     consumer.reset(m_Consumer = new cling::DeclCollector());
     m_CI.reset(CIFactory::createCI("\n", interp->getOptions(), llvmdir,
-                                   std::move(consumer), moduleExtensions));
+                                   std::make_optional(std::move(consumer)),
+                                   moduleExtensions));
 
     if (!m_CI) {
       cling::errs() << "Compiler instance could not be created.\n";

interpreter/cling/lib/Interpreter/Interpreter.cpp
@@ -1036,12 +1036,11 @@ namespace cling {
     std::string llvmDir = parentResourceDir.str();
 
     // arguments for constructing CI
-    auto declCollector = std::make_unique<cling::DeclCollector>();
     const ModuleFileExtensions& moduleExtensions = {};
 
     auto InterpCI = std::unique_ptr<clang::CompilerInstance>(
-        CIFactory::createCI("\n", getOptions(), llvmDir.c_str(),
-                            std::move(declCollector), moduleExtensions,
+        CIFactory::createCI("\n", getOptions(), llvmDir.c_str(), std::nullopt,
+                            moduleExtensions,
                             /*AutoComplete=*/true));
 
     auto CC = ClingCodeCompleter();

interpreter/cling/lib/UserInterface/UserInterface.cpp
@@ -9,6 +9,8 @@
 
 #include "cling/UserInterface/UserInterface.h"
 
+#include "cling/Interpreter/CIFactory.h"
+#include "cling/Interpreter/ClingCodeCompleteConsumer.h"
 #include "cling/Interpreter/Exception.h"
 #include "cling/MetaProcessor/MetaProcessor.h"
 #include "cling/Utils/Output.h"
@@ -53,9 +55,30 @@ namespace {
     std::vector<llvm::LineEditor::Completion> Comps;
     std::vector<std::string> Results;
 
-    MainInterp.codeComplete(Buffer.str(), Pos, Results);
+    std::string resourceDir =
+        MainInterp.getCI()->getHeaderSearchOpts().ResourceDir;
+    // Remove the extra 3 directory names "/lib/clang/3.9.0"
+    llvm::StringRef parentResourceDir =
+        llvm::sys::path::parent_path(llvm::sys::path::parent_path(
+            llvm::sys::path::parent_path(resourceDir)));
+    std::string llvmDir = parentResourceDir.str();
+
+    // arguments for constructing CI
+    const cling::Interpreter::ModuleFileExtensions& moduleExtensions = {};
+
+    auto InterpCI = std::unique_ptr<clang::CompilerInstance>(
+        cling::CIFactory::createCI("\n", MainInterp.getOptions(),
+                                   llvmDir.c_str(), std::nullopt,
+                                   moduleExtensions,
+                                   /*AutoComplete=*/true));
+    auto CC = cling::ClingCodeCompleter();
+    CC.codeComplete(InterpCI.get(), Buffer.str(), 1U, Pos + 1,
+                    MainInterp.getCI(), Results);
+
     for (auto c : Results) {
-      Comps.push_back(llvm::LineEditor::Completion(c, c));
+      if (c.find(CC.Prefix) == 0)
+        Comps.push_back(
+            llvm::LineEditor::Completion(c.substr(CC.Prefix.size()), c));
     }
     return Comps;
   }
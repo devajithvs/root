c47ad437c585e1c834790a088a3f2997080bb8df
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2026-03-11 14:03:50 +0100

    Fix error

--- interpreter/cling/lib/Interpreter/ForwardDeclPrinter.cpp
+++ interpreter/cling/lib/Interpreter/ForwardDeclPrinter.cpp
@@ -1175,16 +1175,7 @@ namespace cling {
           skipDecl(nullptr, "pointee type failed");
           return;
         }
-
-        // FIXME: Provide a NestedNameSpecifier visitor.
-        NestedNameSpecifier Qualifier = MPT->getQualifier();
-        if (NestedNameSpecifier::Kind K = Qualifier.getKind();
-            K == NestedNameSpecifier::Kind::Type)
-          Visit(Qualifier.getAsType());
-        if (MPT->isSugared())
-          Visit(cast<MemberPointerType>(MPT->getCanonicalTypeUnqualified())
-                    ->getQualifier()
-                    .getAsType());
+        VisitNestedNameSpecifier(MPT->getQualifier());
       }
       break;
 
